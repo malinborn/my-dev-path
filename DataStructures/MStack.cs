aade22b2c401a55b40dfcd15228b62c8947ce08a
malinborn <rikkeisy@gmail.com>
2023-01-03 19:10:22 +0300

    initial commit

---
+++ DataStructures/MStack.cs
@@ -0,0 +1,47 @@
+namespace DataStructures;
+
+public class MStack <T>
+{
+    private const int DefaultCapacity = 1024;
+    private T[] _bucket;
+    private int _freeCellIndex;
+    public int Count
+    {
+        get { return _freeCellIndex; }
+    }
+
+    public MStack()
+    {
+        _bucket = new T[DefaultCapacity];
+        _freeCellIndex = 0;
+    }
+
+    public MStack(int userCapacity)
+    {
+        _bucket = new T[userCapacity];
+        _freeCellIndex = 0;
+    }
+
+    public int Push(T value)
+    {
+        if (_freeCellIndex == _bucket.Length)
+        {
+            throw new OverflowException("Stack overflowed with values");
+        }
+
+        _bucket[_freeCellIndex] = value;
+        _freeCellIndex++;
+        return _freeCellIndex - 1;
+    }
+
+    public T Pop()
+    {
+        if (_freeCellIndex == 0)
+        {
+            throw new NullReferenceException("There is no values yet in this stack");
+        }
+
+        _freeCellIndex--;
+        return _bucket[_freeCellIndex];
+    }
+}
---
+++ DataStructures/MStackTests.cs
@@ -0,0 +1,134 @@
+using Xunit;
+
+namespace DataStructures;
+
+public class MStackTests
+{
+    [Fact]
+    public void WhenEmptyStackPushed_ShouldAddValue()
+    {
+        var sut = new MStack<string>();
+
+        var result = sut.Push("Test");
+        
+        Assert.Equal(0, result);
+    }
+    
+    [Fact]
+    public void WhenEmptyStackPopped_ShouldThrowException()
+    {
+        var sut = new MStack<string>();
+
+        Assert.Throws<NullReferenceException>(() => sut.Pop());
+    }
+    
+    [Fact]
+    public void WhenFullStackPushed_ShouldThrowException()
+    {
+        var sut = new MStack<string>(1);
+        
+        sut.Push("Test");
+        
+        Assert.Throws<OverflowException>(() => sut.Push("Test"));
+    }
+
+    [Fact]
+    public void WhenFullStackPopped_ShouldReturnValue()
+    {
+        var sut = new MStack<string>(1);
+        sut.Push("Test");
+
+        var result = sut.Pop();
+        
+        Assert.Equal("Test", result);
+    }
+
+    [Fact]
+    public void When3ValuesPushedAnd2Popped_ShouldReturn2ndPushedValue()
+    {
+        var sut = new MStack<string>(3);
+        sut.Push("First value");
+        sut.Push("Second value");
+        sut.Push("Third value");
+        
+        sut.Pop();
+        var result = sut.Pop();
+        
+        Assert.Equal("Second value", result);
+    }
+    
+    [Fact]
+    public void WhenPoppedStackWithSingleValue_ShouldReturnIt()
+    {
+        var sut = new MStack<string>(1);
+        sut.Push("Test");
+
+        var result = sut.Pop();
+        
+        Assert.Equal("Test", result);
+    }
+
+    [Fact]
+    public void WhenPoppedAllValues_ShouldClearStack()
+    {
+        var sut = new MStack<string>(2);
+        sut.Push("Test");
+        sut.Push("Test");
+
+        sut.Pop();
+        sut.Pop();
+        
+        Assert.Equal(0, sut.Count);
+    }
+
+    [Fact]
+    public void WhenPushed2Values_ShouldContain2Values()
+    {
+        var sut = new MStack<string>(4);
+        
+        sut.Push("Test");
+        sut.Push("Test");
+        
+        Assert.Equal(2, sut.Count);
+    }
+
+    [Fact]
+    public void WhenStackInitializedWithNoArgs_ShouldHave1024Capacity()
+    {
+        var sut = new MStack<int>();
+
+        for (int i = 0; i < 1024; i++)
+        {
+            sut.Push(i);
+        }
+        
+        Assert.Equal(1024, sut.Count);
+    }
+
+    [Fact]
+    public void WhenStackInitializedWithNoArgs_ShouldHaveNotMoreThen1024Capacity()
+    {
+        var sut = new MStack<int>();
+
+        Assert.Throws<OverflowException>(() =>
+        {
+            for (int i = 0; i < 1025; i++)
+            {
+                sut.Push(i);
+            }
+        });
+    }
+
+    [Fact]
+    public void WhenCapacitySet_ShouldDifferFromDefault()
+    {
+        var sut = new MStack<int>(1025);
+        
+        for (int i = 0; i < 1025; i++)
+        {
+            sut.Push(i);
+        }
+        
+        Assert.Equal(1025, sut.Count);
+    }
+}
